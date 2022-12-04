fff221db1503adcabce182a8cdf998fdd69f1fc8
Kihyun Ryu <khryu0610@gmail.com>
2022-12-05 01:53:46 +0900

    feat: 커스텀 이벤트를 활용해 입금, 반환 기능 수정(예외처리 포함)

--- js/app.js
+++ js/app.js
@@ -22,7 +22,10 @@ class Builder {
             this.displayCola(json);
         });
 
-        $btnInsert.addEventListener("click", this.takeMoney.bind(this));
+        $btnInsert.addEventListener("moneyinserted", e => {
+            this.takeMoney(e);
+        });
+        // dispatchEvent 메서드는 이벤트 핸들러를 동기 처리 방식으로 처리한다. 따라서 dispatch 전에 이벤트 핸들러가 등록되어있어야 한다. 여기서는 만족하는듯 하다. (정리 필요)
         $btnReturn.addEventListener("click", this.returnMoney.bind(this));
     }
 
@@ -70,13 +73,36 @@ class Builder {
         $balance.textContent = `${addComma(this.balance)} 원`;
     }
 
-    takeMoney() {
-        this.balance += parseInt($inpInsert.value);
+    takeMoney(event) {
+        // 문제 발생! 클래스를 두 개로 나누다보니 버튼에 달린 이벤트 실행 순서가 뒤죽박죽이어서 제대로 동작하지 않는 문제 발생. 또한 예외처리를 두 클래스에서 모두 해줘야한다는 단점도 있음. 따라서 조금 더 상호작용을 흉내내기 위해 사용자가 돈을 성공적으로 입금한 후(== 소지금의 변화가 생길 경우, 혹은 유니크한 사용자 정의 이벤트) 자판기의 이벤트를 발생시키도록 수정하겠음.
+        console.log(event.detail.amount);
+        this.balance += parseInt(event.detail.amount);
         this.updateBalance();
         $inpInsert.value = null;
+
+        // console.log("소지금: " + parseInt(deleteComma($myMoney.textContent)));
+        // const insertedMoney = parseInt($inpInsert.value);
+        // if (insertedMoney) {
+        //     if (
+        //         insertedMoney <= parseInt(deleteComma($myMoney.textContent)) &&
+        //         insertedMoney > 0
+        //     ) {
+        //         this.balance += parseInt($inpInsert.value);
+        //         this.updateBalance();
+        //     } else {
+        //         alert("문제 발생!");
+        //         $inpInsert.value = null;
+        //         return;
+        //     }
+        //     $inpInsert.value = null;
+        // }
     }
 
     returnMoney() {
+        const spitMoney = new CustomEvent("moneyspit", {
+            detail: { amount: this.balance },
+        });
+        $btnReturn.dispatchEvent(spitMoney);
         this.balance = 0;
         this.updateBalance();
     }
@@ -90,21 +116,38 @@ class Person {
     ready() {
         this.updateMoney();
 
-        $btnInsert.addEventListener("click", this.insertMoney.bind(this)); //주의
-        $btnReturn.addEventListener("click", this.takeChange.bind(this)); //주의
+        $btnInsert.addEventListener("click", () => {
+            this.insertMoney();
+        }); //주의
+        $btnReturn.addEventListener("moneyspit", e => {
+            this.takeChange(e);
+        }); //주의
     }
 
     insertMoney() {
-        this.myMoney -= parseInt($inpInsert.value);
-        this.updateMoney();
+        const insertedMoney = parseInt($inpInsert.value);
+
+        if (insertedMoney) {
+            if (insertedMoney <= this.myMoney && insertedMoney > 0) {
+                const moneyInserted = new CustomEvent("moneyinserted", {
+                    detail: { amount: insertedMoney },
+                });
+                this.myMoney -= parseInt($inpInsert.value);
+                this.updateMoney();
+                $btnInsert.dispatchEvent(moneyInserted);
+            } else {
+                alert("소지금이 부족합니다.");
+                $inpInsert.value = null;
+            }
+        }
     }
 
     updateMoney() {
         $myMoney.textContent = `${addComma(this.myMoney)} 원`;
     }
 
-    takeChange() {
-        this.myMoney += parseInt(deleteComma($balance.textContent)); // builder의 balance로 바꿀 수 있을까?
+    takeChange(event) {
+        this.myMoney += event.detail.amount; // builder의 balance로 바꿀 수 있을까?
         this.updateMoney();
     }
 }
